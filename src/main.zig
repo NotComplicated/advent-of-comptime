a0830a9373dd60acde38b04295e20f3efbd8fd59
Andres Dejesus <andresdejesus123@gmail.com>
2025-12-05 21:38:03 -0600

    simpler printing

--- src/main.zig
+++ src/main.zig
@@ -4,15 +4,24 @@ const challenges = @import("advent_of_comptime");
 pub fn main() !void {
     comptime var out: []const u8 = "";
     inline for (1.., challenges.results) |i, results| {
-        out = out ++ std.fmt.comptimePrint("Challenge {d:02}:\n", .{i});
-        out = out ++ std.fmt.comptimePrint("\tPart 1 (sample): {!}\n", .{results.part1_sample});
-        out = out ++ std.fmt.comptimePrint("\tPart 1 (input): {!}\n", .{results.part1_input});
-        out = out ++ std.fmt.comptimePrint("\tPart 2 (sample): {!}\n", .{results.part2_sample});
-        out = out ++ std.fmt.comptimePrint("\tPart 2 (input): {!}\n\n", .{results.part2_input});
+        out = out ++ std.fmt.comptimePrint(
+            \\Challenge {d:02}:
+            \\  Part 1 (sample): {!}
+            \\  Part 1 (input): {!}
+            \\  Part 2 (sample): {!}
+            \\  Part 2 (input): {!}
+            \\
+            \\
+        ,
+            .{
+                i,
+                results.part1_sample,
+                results.part1_input,
+                results.part2_sample,
+                results.part2_input,
+            },
+        );
     }
-    const stdout = std.fs.File.stdout();
-    var buf: [out.len]u8 = undefined;
-    var writer = stdout.writer(&buf);
+    var writer = std.fs.File.stdout().writer(&.{});
     try writer.interface.writeAll(out);
-    try writer.interface.flush();
 }
