30431570b5aa8f9bde2a4b21740151bec2f9bdc6
Andres Dejesus <andresdejesus123@gmail.com>
2025-12-06 11:04:27 -0600

    slightly better day 4

--- src/days/day04.zig
+++ src/days/day04.zig
@@ -17,8 +17,29 @@ pub const sample =
 
 pub fn part1(input: []const u8) !i64 {
     const cols = std.mem.indexOfScalar(u8, input, '\n').? + 1;
-    var rolls: [input.len]u8 = input[0..].*;
-    return remove(rolls[0..], cols);
+    var sum = 0;
+    for (0..input.len) |i| {
+        if (input[i] != '@') continue;
+        const row = i / cols;
+        const col = i % cols;
+        var count = 0;
+        if (row > 0) {
+            const x = (row - 1) * cols;
+            if (col > 0 and input[x + col - 1] == '@') count += 1;
+            if (input[x + col] == '@') count += 1;
+            if (col < cols - 1 and input[x + col + 1] == '@') count += 1;
+        }
+        if (col > 0 and input[row * cols + col - 1] == '@') count += 1;
+        if (col < cols - 1 and input[row * cols + col + 1] == '@') count += 1;
+        if (row < input.len / cols - 1) {
+            const x = (row + 1) * cols;
+            if (col > 0 and input[x + col - 1] == '@') count += 1;
+            if (input[x + col] == '@') count += 1;
+            if (col < cols - 1 and input[x + col + 1] == '@') count += 1;
+        }
+        if (count < 4) sum += 1;
+    }
+    return sum;
 }
 
 pub fn part2(input: []const u8) !i64 {
@@ -29,37 +50,34 @@ pub fn part2(input: []const u8) !i64 {
         const removed = remove(rolls[0..], cols);
         if (removed == 0) return sum;
         sum += removed;
-        for (&rolls) |*roll| if (roll.* & 1 == 1) {
-            roll.* = '.';
-        };
     }
 }
 
-pub fn remove(rolls: []u8, cols: usize) usize {
-    var sum = 0;
-    for (rolls, 0..) |*c, i| {
-        if (c.* & '@' == 0) continue;
+fn remove(rolls: []u8, cols: usize) i64 {
+    var removed = 0;
+    for (0..rolls.len) |i| {
+        if (rolls[i] != '@') continue;
         const row = i / cols;
         const col = i % cols;
         var count = 0;
         if (row > 0) {
             const x = (row - 1) * cols;
-            if (col > 0 and rolls[x + col - 1] & '@' == '@') count += 1;
-            if (rolls[x + col] & '@' == '@') count += 1;
-            if (col < cols - 1 and rolls[x + col + 1] & '@' == '@') count += 1;
+            if (col > 0 and rolls[x + col - 1] == '@') count += 1;
+            if (rolls[x + col] == '@') count += 1;
+            if (col < cols - 1 and rolls[x + col + 1] == '@') count += 1;
         }
-        if (col > 0 and rolls[row * cols + col - 1] & '@' == '@') count += 1;
-        if (col < cols - 1 and rolls[row * cols + col + 1] & '@' == '@') count += 1;
+        if (col > 0 and rolls[i - 1] == '@') count += 1;
+        if (col < cols - 1 and rolls[i + 1] == '@') count += 1;
         if (row < rolls.len / cols - 1) {
             const x = (row + 1) * cols;
-            if (col > 0 and rolls[x + col - 1] & '@' == '@') count += 1;
-            if (rolls[x + col] & '@' == '@') count += 1;
-            if (col < cols - 1 and rolls[x + col + 1] & '@' == '@') count += 1;
+            if (col > 0 and rolls[x + col - 1] == '@') count += 1;
+            if (rolls[x + col] == '@') count += 1;
+            if (col < cols - 1 and rolls[x + col + 1] == '@') count += 1;
         }
         if (count < 4) {
-            c.* |= 1;
-            sum += 1;
+            rolls[i] = '.';
+            removed += 1;
         }
     }
-    return sum;
+    return removed;
 }
