34cc0f536491cf7e7aaaac34c7cc0cfa3be2a10a
Andres Dejesus <andresdejesus123@gmail.com>
2025-12-12 21:02:59 -0600

    day 8 FINALLY done

--- src/days/day08.zig
+++ src/days/day08.zig
@@ -25,8 +25,8 @@ pub const sample =
     \\
 ;
 
-const Junction = [3]f32;
-const Connection = struct { dist: f32, first: Junction, second: Junction };
+const Junction = struct { x: f64, y: f64, z: f64, circuit: ?usize = null };
+const Connection = struct { dist: f64, first: *Junction, second: *Junction };
 
 pub fn part1(input: []const u8) !i64 {
     return inner(input, .one);
@@ -37,63 +37,56 @@ pub fn part2(input: []const u8) !i64 {
 }
 
 fn inner(input: []const u8, part: enum { one, two }) !u32 {
-    const units, const unit_size = u: {
-        var unit_size = std.math.maxInt(usize);
-        var largest = 0;
-        var lines = utils.iterLines(input);
-        while (lines.next()) |line| {
-            if (line.len == 0) break;
-            const next_line = lines.next().?;
-            var fields = utils.iterCommas(line);
-            var next_fields = utils.iterCommas(next_line);
-            var junction: Junction = undefined;
-            for (&junction) |*xyz|
-                xyz.* = try std.fmt.parseFloat(f32, fields.next().?);
-            var next_junction: Junction = undefined;
-            for (&next_junction) |*xyz|
-                xyz.* = try std.fmt.parseFloat(f32, next_fields.next().?);
-            const dx = junction[0] - next_junction[0];
-            const dy = junction[1] - next_junction[1];
-            const dz = junction[2] - next_junction[2];
-            unit_size = @min(unit_size, @ceil(@sqrt(dx * dx + dy * dy + dz * dz)));
-            largest = @max(
-                largest,
-                junction[0],
-                junction[1],
-                junction[2],
-                next_junction[0],
-                next_junction[1],
-                next_junction[2],
-            );
-        }
-        unit_size = @ceil(unit_size * 1.1);
-        break :u .{ largest / unit_size + 1, unit_size };
-    };
-    var buckets: [units + 2][units + 2][units + 2][]const Junction = undefined;
+    var junctions: [1000]Junction = undefined;
+    var junctions_len = 0;
+    var unit_size = std.math.maxInt(usize);
+    var largest = 0;
+    var lines = utils.iterLines(input);
+    while (lines.next()) |line| {
+        if (line.len == 0) break;
+        const next_line = lines.next().?;
+        var fields = utils.iterCommas(line);
+        var next_fields = utils.iterCommas(next_line);
+        junctions[junctions_len] = .{
+            .x = try std.fmt.parseFloat(f64, fields.next().?),
+            .y = try std.fmt.parseFloat(f64, fields.next().?),
+            .z = try std.fmt.parseFloat(f64, fields.next().?),
+        };
+        junctions[junctions_len + 1] = .{
+            .x = try std.fmt.parseFloat(f64, next_fields.next().?),
+            .y = try std.fmt.parseFloat(f64, next_fields.next().?),
+            .z = try std.fmt.parseFloat(f64, next_fields.next().?),
+        };
+        const dx = junctions[junctions_len].x - junctions[junctions_len + 1].x;
+        const dy = junctions[junctions_len].y - junctions[junctions_len + 1].y;
+        const dz = junctions[junctions_len].z - junctions[junctions_len + 1].z;
+        unit_size = @min(unit_size, @ceil(@sqrt(dx * dx + dy * dy + dz * dz)));
+        largest = @max(
+            largest,
+            junctions[junctions_len].x,
+            junctions[junctions_len].y,
+            junctions[junctions_len].z,
+            junctions[junctions_len + 1].x,
+            junctions[junctions_len + 1].y,
+            junctions[junctions_len + 1].z,
+        );
+        junctions_len += 2;
+    }
+    unit_size = @ceil(unit_size * 1.2);
+    const units = largest / unit_size + 1;
+    var buckets: [units + 2][units + 2][units + 2][]const *Junction = undefined;
     for (&buckets) |*x| for (x) |*xy| for (xy) |*xyz| {
         xyz.* = &.{};
     };
-    const junction_count = jc: {
-        var lines = utils.iterLines(input);
-        var jc = 0;
-        while (lines.next()) |line| {
-            if (line.len == 0) break;
-            jc += 1;
-            var fields = utils.iterCommas(line);
-            var junction: Junction = undefined;
-            for (&junction) |*field| {
-                field.* = try std.fmt.parseFloat(f32, fields.next().?);
-            }
-            const x = @divFloor(junction[0], unit_size) + 1;
-            const y = @divFloor(junction[1], unit_size) + 1;
-            const z = @divFloor(junction[2], unit_size) + 1;
-            buckets[x][y][z] = buckets[x][y][z] ++ .{junction};
-        }
-        break :jc jc;
-    };
-    const max_conns = switch (part) {
-        .one => if (input.ptr == sample.ptr) 10 else 1000,
-        .two => if (input.ptr == sample.ptr) 40 else 3000,
+    for (junctions[0..junctions_len]) |*junction| {
+        const x = @divFloor(junction.x, unit_size) + 1;
+        const y = @divFloor(junction.y, unit_size) + 1;
+        const z = @divFloor(junction.z, unit_size) + 1;
+        buckets[x][y][z] = buckets[x][y][z] ++ .{junction};
+    }
+    const max_conns, const max_circuits = switch (part) {
+        .one => if (input.ptr == sample.ptr) .{ 10, 5 } else .{ 1000, 300 },
+        .two => if (input.ptr == sample.ptr) .{ 40, 10 } else .{ 4800, 800 },
     };
     var conns: [max_conns]Connection = undefined;
     var conns_len = 0;
@@ -101,10 +94,10 @@ fn inner(input: []const u8, part: enum { one, two }) !u32 {
         for (buckets[x0][y0][z0]) |first| {
             for (x0 - 1..x0 + 2) |x1| for (y0 - 1..y0 + 2) |y1| for (z0 - 1..z0 + 2) |z1| {
                 for (buckets[x1][y1][z1]) |second| {
-                    if (first[0] == second[0] and first[1] == second[1] and first[2] == second[2]) continue;
-                    const dx = first[0] - second[0];
-                    const dy = first[1] - second[1];
-                    const dz = first[2] - second[2];
+                    if (first.x == second.x and first.y == second.y and first.z == second.z) continue;
+                    const dx = first.x - second.x;
+                    const dy = first.y - second.y;
+                    const dz = first.z - second.z;
                     const dist = @sqrt(dx * dx + dy * dy + dz * dz);
                     const at = if (conns_len > 0 and dist < conns[conns_len - 1].dist) at: {
                         var lower = 0;
@@ -117,7 +110,7 @@ fn inner(input: []const u8, part: enum { one, two }) !u32 {
                     } else conns_len;
                     if (at < conns_len) {
                         const prev_first = conns[at].first;
-                        if (prev_first[0] != second[0] or prev_first[1] != second[1] or prev_first[2] != second[2]) {
+                        if (prev_first.x != second.x or prev_first.y != second.y or prev_first.z != second.z) {
                             if (conns_len < conns.len) {
                                 @memmove(conns[at + 1 .. conns_len + 1], conns[at..conns_len]);
                                 conns_len += 1;
@@ -132,52 +125,57 @@ fn inner(input: []const u8, part: enum { one, two }) !u32 {
             };
         }
     };
-    var circuits: [conns_len][]const Junction = .{&.{}} ** conns_len;
-    var circuits_len = 1;
+    var circuits: [max_circuits][conns_len]*Junction = undefined;
+    var circuit_lens: [max_circuits]usize = undefined;
+    var circuits_len = 0;
     for (conns[0..conns_len]) |conn| {
-        var found: [2]?usize = .{ null, null };
-        var first_empty: ?usize = null;
-        for (.{ conn.first, conn.second }, &found) |junction, *at| {
-            circuits: for (0..circuits_len) |j| {
-                if (first_empty == null and circuits[j].len == 0) first_empty = j;
-                for (circuits[j]) |prev| {
-                    if (junction[0] == prev[0] and junction[1] == prev[1] and junction[2] == prev[2]) {
-                        at.* = j;
-                        break :circuits;
-                    }
+        var new_len = 0;
+        if (conn.first.circuit) |first_circuit| {
+            if (conn.second.circuit) |second_circuit| {
+                if (first_circuit != second_circuit) {
+                    for (circuits[second_circuit][0..circuit_lens[second_circuit]]) |junction|
+                        junction.circuit = first_circuit;
+                    @memcpy(
+                        circuits[first_circuit][circuit_lens[first_circuit]..].ptr,
+                        circuits[second_circuit][0..circuit_lens[second_circuit]],
+                    );
+                    circuit_lens[first_circuit] += circuit_lens[second_circuit];
+                    circuit_lens[second_circuit] = 0;
+                    conn.second.circuit = first_circuit;
+                    new_len = circuit_lens[first_circuit];
                 }
+            } else {
+                circuits[first_circuit][circuit_lens[first_circuit]] = conn.second;
+                circuit_lens[first_circuit] += 1;
+                conn.second.circuit = first_circuit;
+                new_len = circuit_lens[first_circuit];
             }
+        } else if (conn.second.circuit) |second_circuit| {
+            circuits[second_circuit][circuit_lens[second_circuit]] = conn.first;
+            circuit_lens[second_circuit] += 1;
+            conn.first.circuit = second_circuit;
+            new_len = circuit_lens[second_circuit];
+        } else {
+            circuits[circuits_len][0] = conn.first;
+            circuits[circuits_len][1] = conn.second;
+            circuit_lens[circuits_len] = 2;
+            conn.first.circuit = circuits_len;
+            conn.second.circuit = circuits_len;
+            circuits_len += 1;
         }
-        const size = if (found[0]) |first| s: {
-            if (found[1]) |second| {
-                if (first != second) {
-                    circuits[first] = circuits[first] ++ circuits[second];
-                    circuits[second] = &.{};
-                }
-            } else circuits[first] = circuits[first] ++ .{conn.second};
-            break :s circuits[first].len;
-        } else if (found[1]) |second| s: {
-            circuits[second] = circuits[second] ++ .{conn.first};
-            break :s circuits[second].len;
-        } else s: {
-            circuits[first_empty.?] = &.{ conn.first, conn.second };
-            circuits_len = @max(circuits_len, first_empty.? + 2);
-            break :s 0;
-        };
-        if (part == .two and size == junction_count)
-            return conn.first[0] * conn.second[0];
+        if (part == .two and new_len == junctions_len) return conn.first.x * conn.second.x;
     }
     if (part == .two) return error.NotEnoughConnections;
     var maxes: [3]usize = .{1} ** 3;
-    for (circuits[0..circuits_len]) |circuit| {
-        if (circuit.len > maxes[0]) {
-            maxes[0] = circuit.len;
+    for (circuit_lens[0..circuits_len]) |circuit_len| {
+        if (circuit_len > maxes[0]) {
+            maxes[2] = maxes[1];
             maxes[1] = maxes[0];
+            maxes[0] = circuit_len;
+        } else if (circuit_len > maxes[1]) {
             maxes[2] = maxes[1];
-        } else if (circuit.len > maxes[1]) {
-            maxes[1] = circuit.len;
-            maxes[2] = maxes[1];
-        } else if (circuit.len > maxes[2]) maxes[2] = circuit.len;
+            maxes[1] = circuit_len;
+        } else if (circuit_len > maxes[2]) maxes[2] = circuit_len;
     }
     return maxes[0] * maxes[1] * maxes[2];
 }
